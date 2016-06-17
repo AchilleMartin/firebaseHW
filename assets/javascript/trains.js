c7f515629babd5b5034bc4435c814dc3f77da04e
Todd Germaine <trgermaine@gmail.com>
2016-06-17 15:48:41 -0400

    fixed appending to table issue

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -1,23 +1,18 @@
 $( document ).ready(function() {
 
-
-var clicked = 0;
+var train = '';
+var destination = '';
+var time = '';
+var frequency = '';
 
 //Firebase URL Listed Below
 var dataRef = new Firebase("https://traintimenyc.firebaseio.com/");
 
-console.log(clicked);
-
-
 	$('#addTrainBtn').on('click', function() {
-
-		var train = $('#trainNameInput').val();
-		var destination = $('#destinationInput').val();
-		var time = $('#timeInput').val();
-		var frequency = $('#frequencyInput').val();
-
-		console.log('Yes I Work');
-		clicked++;
+	  train = $('#trainNameInput').val();
+		destination = $('#destinationInput').val();
+		time = $('#timeInput').val();
+		frequency = $('#frequencyInput').val();
 
 		dataRef.push({
 
@@ -34,7 +29,50 @@ console.log(clicked);
 
 	});
 
-		dataRef.on("value", function(snapshot) {
+		dataRef.on("child_added", function(snapshot) {
+
+		// Logs all of the user-input data
+		console.log(snapshot.val().train + " = train");
+		console.log(snapshot.val().destination + " = destination");
+		console.log(snapshot.val().time + " = time");
+		console.log(snapshot.val().frequency +" = frequency");
+
+		//
+		// var train = $('td').append(snapshot.val().train + "train");
+		// var destination = $('td').append(snapshot.val().destination + "destination");
+		// var time = $('td').append(snapshot.val().time + "time");
+		// var frequency = $('td').append(snapshot.val().frequency + "frequency");
+
+
+		var trainSubmit = [snapshot.val().train, snapshot.val().destination, snapshot.val().time, snapshot.val().frequency];
+
+		// Creating a row when userinput is submitted
+		var row = $('<tr>')
+
+		// For loop to go through all of the values for userInput
+
+		for (var i = 0; i < trainSubmit.length; i++) {
+
+				// Creating variable to make new data cell for table
+				var	datacell = $('<td>')
+
+				// Pushes Aaray to the datacell
+				datacell.html(trainSubmit[i]);
+
+				//Creating an id to use later if necessary
+				datacell.attr('id', i);
+
+				//Pushing the datacell to the row
+				row.append(datacell);
+
+		}
+
+		//Appends row to the table id div
+
+		$('#userInput').append(row);
+
+
+		return false;
 
 			}, function (errorObject) {
 					console.log('The read failed' + errorObject.code);
